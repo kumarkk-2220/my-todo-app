5e9144745e32ba48bc1d4d771165d1acb4953d0c
Prasanna Kumar (kumarkk-2220) <kumarkk.2220@gmail.com>
2023-03-21 01:10:48 +0530

    Updates to the main 'py' file to mitigate errors in web app deployment

--- Webapp/Webapp-ToDo/web_app.py
+++ Webapp/Webapp-ToDo/web_app.py
@@ -5,8 +5,8 @@ todos = functions.get_todos()
 
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    todos.append(todo)
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
     functions.write_todos(todos_arg=todos)
 
 
@@ -25,5 +25,3 @@ for index, todo in enumerate(todos):
 
 
 st.text_input(label="Enter a ToDo", placeholder="Add a new todo...", on_change=add_todo, key='new_todo')
-
-st.session_state
